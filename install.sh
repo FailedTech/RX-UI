f8d9741cb873275a131dbdf520c22a8930732730
FailedTech <failedtech69@gmail.com>
2023-08-21 12:04:47 +0330

    08/21/2023

--- install.sh
+++ install.sh
@@ -18,6 +18,21 @@ yellow='\033[0;33m'
 #################### Clash Meta Session #############
 RX_UI_VERSION='0.0.1'
 CM_Core_PATH='/usr/local/bin/clash-meta'
+CM_SERVICE_PATH='/etc/systemd/system/clash-meta.service'
+CM_DOWNLOAD_PATH='/usr/local/Clash-Meta/Download/'
+CM_CONFIG_PATH='/usr/local/Clash-Meta/Config/'
+
+CM_Directory_setup(){
+    if [[ -d "/usr/local/Clash-Meta" ]]; then
+        (
+            mkdir -p ${CM_CONFIG_PATH} ${CM_DOWNLOAD_PATH}
+        )
+    else
+        (
+        mkdir -p "/usr/local/Clash-Meta" ${CM_CONFIG_PATH} ${CM_DOWNLOAD_PATH}
+        )
+    fi
+}
 
 CM_Core_latest_version() {
     latest_release=$(curl -s "https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/latest" | grep -o '"tag_name": "v.*"' | cut -d '"' -f4 | sed 's/v//')
@@ -34,12 +49,12 @@ CM_Core_installed_version() {
 }
 
 CM_Core_download(){
-    curl -Lo 'clash-meta.gz' "https://github.com/MetaCubeX/Clash.Meta/releases/download/v$(CM_Core_latest_version)/clash.meta-linux-amd64-compatible-v$(CM_Core_latest_version).gz"
-    gunzip clash-meta.gz &&
-    mv clash-meta $CM_Core_PATH && chmod +x $CM_Core_PATH
+    curl -Lo ${CM_DOWNLOAD_PATH}clash-meta.gz "https://github.com/MetaCubeX/Clash.Meta/releases/download/v$(CM_Core_latest_version)/clash.meta-linux-amd64-compatible-v$(CM_Core_latest_version).gz"
+    gunzip ${CM_DOWNLOAD_PATH}clash-meta.gz &&
+    mv ${CM_DOWNLOAD_PATH}clash-meta $CM_Core_PATH && chmod +x $CM_Core_PATH
 }
 
-CM_Core_setup() {
+CM_Core_install() {
     latest_version=$(CM_Core_latest_version)
     installed_version=$(CM_Core_installed_version)
     echo -e "$installed_version $latest_version"
@@ -56,19 +71,92 @@ CM_Core_setup() {
     fi
 }
 
+CM_Core_uninstall(){
+    rm -rf $CM_Core_PATH 
+}
+
+CM_Core_status(){
+   $CM_Core_PATH status
+}
+
+CM_Service_default(){
+    echo -e "[Unit]
+Description=Clash-Meta Daemon, Another Clash Kernel.
+After=network.target NetworkManager.service systemd-networkd.service iwd.service
+
+[Service]
+Type=simple
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE
+Restart=always
+ExecStartPre=/usr/bin/sleep 1s
+ExecStart=/usr/local/bin/clash-meta -d /usr/local/Clash-Meta/Config
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/clash-meta.service
+}
+
+CM_Service_install(){
+    if [[ -f "$CM_SERVICE_PATH" ]]; then
+        CM_Service_default
+    else
+        sudo touch /etc/systemd/system/clash-meta.service
+        CM_Service_default
+    fi
+}
+
+CM_Service_start(){
+    sudo systemctl enable clash-meta
+    sudo systemctl start clash-meta
+}
+
+CM_Service_stop(){
+    sudo systemctl stop clash-meta
+}
+
+CM_Service_restart(){
+    sudo systemctl daemon-reload
+    sudo systemctl restart clash-meta
+}
+
+CM_Service_status(){
+    sudo systemctl status clash-meta
+}
+
+CM_Config_default(){
+
+}
+
+CM_Config_install(){
+    if [[ -f "${CM_CONFIG_PATH}config.yaml" ]]; then
+        CM_Config_default
+    else
+        sudo touch "${CM_CONFIG_PATH}config.yaml"
+        CM_Config_default
+    fi
+}
+
+CM_Config_edit(){
+    sudo nano ${CM_CONFIG_PATH}config.yaml
+}
+
+CM_Config_check(){
+    sudo ${CM_Core_PATH} ${CM_CONFIG_PATH}config.yaml
+}
+
 menu() {
     echo -e "
 ${green}RX-UI-v${RX_UI_VERSION} Management Script${plain}
 ${pink}>>>>>>>>>>   Exit  <<<<<<<<<<${plain}
 ${green}0.${plain} Exit
 ${pink}>>>>>>>>>>   Core  <<<<<<<<<<${plain}
-${green}1.${plain} Install       ${green}2.${plain} Update
-${green}3.${plain} Uninstall     ${green}4.${plain} Status
+${green}1.${plain} Install/Update
+${green}2.${plain} Uninstall     ${green}3.${plain} Status
 ${pink}>>>>>>>>>> Service <<<<<<<<<<${plain}
-${green}5.${plain} Start         ${green}6.${plain} Stop
-${green}7.${plain} Restart       ${green}8.${plain} Status
+${green}4.${plain} Start         ${green}5.${plain} Stop
+${green}6.${plain} Restart       ${green}7.${plain} Status
 ${pink}>>>>>>>>>>  Config  <<<<<<<<<<${plain}
-${green}9.${plain} Edit Config   ${green}10.${plain} Check Config
+${green}8.${plain} Edit Config   ${green}9.${plain} Check Config
       "
 
     read -p "Please enter your choice [0-21]: " num
@@ -78,7 +166,37 @@ ${green}9.${plain} Edit Config   ${green}10.${plain} Check Config
             exit 0
             ;;
         1)
-            CM_Core_setup && menu
+            CM_Directory_setup && CM_Core_install && menu
+            ;;
+        2)
+            CM_Core_uninstall && menu
+            ;;
+        3)
+            CM_Core_status && menu
+            ;;
+        4)
+            CM_Service_start && menu
+            ;;
+        5)
+            CM_Service_stop && menu
+            ;;
+        6)
+            CM_Service_restart && menu
+            ;;
+        7)
+            CM_Service_status && menu
+            ;;
+        8)
+            CM_Config_edit && menu
+            ;;
+        9)
+            CM_Config_check && menu
+            ;;
+        10)
+            menu
+            ;;
+        11)
+            menu
             ;;
         *)
             echo "Please enter a valid option [0-21]"
