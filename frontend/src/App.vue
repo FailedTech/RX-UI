7fe86b22c2ffad4f30bbd5b46aac2a69f813012b
FailedTech <FailedTech69@gmail.com>
2025-07-21 14:54:15 +0330

    07/21/2025 02:54 PM

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
-
 </script>
 
 <template>
---
+++ frontend/src/components/icons/align-left-mobile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <img
+        src="@/assets/svg/align-left.svg"
+        alt="Align Left Mobile Icon"
+        aria-hidden="true"
+        width="30"
+        height="20"
+        role="img"
+        aria-label="Align Left Mobile"
+        class="border-2 border-gray-500 rounded-md"
+      />
+    </div>
+</template>
+
